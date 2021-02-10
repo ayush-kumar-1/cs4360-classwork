25fc053e437b19d7a9988728db33529490b6b237
ayush-kumar-1 <58648531+ayush-kumar-1@users.noreply.github.com>
2021-02-10 13:15:02 -0500

    Updated Collinearity file

--- src/main/scala/feb9/Collinearity.scala
+++ src/main/scala/feb9/Collinearity.scala
@@ -2,6 +2,7 @@ package feb9
 
 import scalation.analytics.Regression
 import scalation.linalgebra.{VectoD, VectorD, MatriD, MatrixD}
+import scalation.stat.StatVector.corr
 
 object Collinearity extends App {
 
@@ -9,12 +10,18 @@ object Collinearity extends App {
     val x = new MatrixD((4,3),  1, 1, 1, 
                                 1, 2, 2,
                                 1, 3, 3,
-                                1, 4, -1)
+                                1, 4, 0)
 
     val y = VectorD(1.0, 3.0, 3.0, 4.0)
 
     println(s"x = $x")
-    println(s"y = $y")
+    println(s"corr (x) = ${corr(x)}")
+
+    val rg = new Regression(x, y)
+    rg.train().eval()
+
+    println(rg.report)
+    println(rg.summary) 
 
     
 
